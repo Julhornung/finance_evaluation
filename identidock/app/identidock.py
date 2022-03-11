10a7bdcba606ad7e4526c72b6c3364bef834de0b
julius <julhornung@gmail.com>
2022-03-11 14:35:02 +0100

    changes in identidock.py

--- identidock/app/identidock.py
+++ identidock/app/identidock.py
@@ -1,11 +1,19 @@
 from flask import Flask
+from data_processing import get_data
+
+df = get_data()
+
 app = Flask(__name__)
 
 @app.route('/')
 
-def hello_world():
-  return 'Hello Docker!\n'
+def hello_world(df):
+
+  return(df.head())
+
+  # return 'Hello Docker!\n'
 
 if __name__ == '__main__':
+
   app.run(debug = True, host = '0.0.0.0')
 
